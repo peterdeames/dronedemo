f9b41043d4e64ccdcb45f49f590ed8e82312e596
Peter Eames <peterdeames@gmail.com>
2022-10-15 21:28:30 +0100

    changing lib

--- test_flight.py
+++ test_flight.py
@@ -3,8 +3,7 @@ Run Test flight to check the connection and the battery levels
 '''
 import logging
 import sys
-from tellodji import Tello
-
+from droneblocks.DroneBlocksTello import DroneBlocksTello
 
 
 # The different levels of logging from the highest to the lowest urgency are:
@@ -14,13 +13,13 @@ logging.basicConfig(stream=sys.stderr, level=logging.INFO,
 
 def main():
     """ This function will run a test flight to check the drone is connected """
-    tello = Tello(debug=True)
-    #try:
-    #    logging.info('Connecting to drone')
-    #    tello.connect()
-    #except:
-    #    logging.error('Connection failed')
-    #    sys.exit(1)
+    tello = DroneBlocksTello()
+    try:
+        logging.info('Connecting to drone')
+        tello.connect()
+    except:
+        logging.error('Connection failed')
+        sys.exit(1)
     bat = tello.get_battery()
     logging.info('The battery has %d percent left', bat)
     if bat >= 5:
